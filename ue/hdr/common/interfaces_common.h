4b72c8a6119ee86f7afcef5e211aae95aa38a94d
Ismael Gomez <ismagom@gmail.com>
2016-05-31 00:01:55 +0200

    added missing file

---
+++ ue/hdr/common/interfaces_common.h
@@ -0,0 +1,17 @@
+
+
+#ifndef INTERFACE_COMMON_H
+#define INTERFACE_COMMON_H
+
+namespace srslte {
+
+
+class read_pdu_interface
+{
+public:
+  virtual int read_pdu(uint32_t lcid, uint8_t *payload, uint32_t requested_bytes) = 0; 
+};
+
+}
+
+#endif
