5ea5e3a7a7168c191b11a8382ee73ed336d90b86
Ismael Gomez <ismagom@gmail.com>
2016-11-11 18:40:48 +0100

    Ignore TA commands and warn. Need to check

--- ue/src/phy/phy.cc
+++ ue/src/phy/phy.cc
@@ -179,8 +179,8 @@ void phy::set_timeadv_rar(uint32_t ta_cmd) {
 
 void phy::set_timeadv(uint32_t ta_cmd) {
   n_ta = srslte_N_ta_new(n_ta, ta_cmd);
-  sf_recv.set_time_adv_sec(((float) n_ta)*SRSLTE_LTE_TS);  
-  Info("PHY:   Set TA: ta_cmd: %d, n_ta: %d, ta_usec: %.1f\n", ta_cmd, n_ta, ((float) n_ta)*SRSLTE_LTE_TS*1e6);
+  //sf_recv.set_time_adv_sec(((float) n_ta)*SRSLTE_LTE_TS);  
+  Warning("Not supported: Set TA: ta_cmd: %d, n_ta: %d, ta_usec: %.1f\n", ta_cmd, n_ta, ((float) n_ta)*SRSLTE_LTE_TS*1e6);
 }
 
 void phy::configure_prach_params()
