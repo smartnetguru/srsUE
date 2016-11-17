ac60d1c18080097ccfd5b1caf6ce65a26ae3762e
Paul Sutton <paul@softwareradiosystems.com>
2016-11-17 09:02:07 +0000

    Added new elements/utilities, removed typedefs.h

--- liblte/hdr/liblte_common.h
+++ liblte/hdr/liblte_common.h
@@ -40,7 +40,9 @@
                               INCLUDES
 *******************************************************************************/
 
-#include "typedefs.h"
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
 #include <string.h>
 
 /*******************************************************************************
@@ -48,6 +50,7 @@
 *******************************************************************************/
 
 // FIXME: This was chosen arbitrarily
+#define LIBLTE_ASN1_OID_MAXSUBIDS 128
 #define LIBLTE_MAX_MSG_SIZE_BITS  102048
 #define LIBLTE_MAX_MSG_SIZE_BYTES 12756
 #define LIBLTE_MSG_HEADER_OFFSET  1024
@@ -56,12 +59,35 @@
                               TYPEDEFS
 *******************************************************************************/
 
+typedef int8_t    int8;
+typedef uint8_t   uint8;
+typedef int16_t   int16;
+typedef uint16_t  uint16;
+typedef int32_t   int32;
+typedef uint32_t  uint32;
+typedef int64_t   int64;
+typedef uint64_t  uint64;
+
 typedef enum{
-    LIBLTE_SUCCESS = 0,
-    LIBLTE_ERROR_INVALID_INPUTS,
-    LIBLTE_ERROR_DECODE_FAIL,
-    LIBLTE_ERROR_INVALID_CRC,
+  LIBLTE_SUCCESS = 0,
+  LIBLTE_ERROR_INVALID_INPUTS,
+  LIBLTE_ERROR_ENCODE_FAIL,
+  LIBLTE_ERROR_DECODE_FAIL,
+  LIBLTE_ERROR_INVALID_CRC,
+  LIBLTE_ERROR_N_ITEMS
 }LIBLTE_ERROR_ENUM;
+static const char liblte_error_text[LIBLTE_ERROR_N_ITEMS][64] = {
+  "Invalid inputs",
+  "Encode failure",
+  "Decode failure",
+};
+
+typedef void* LIBLTE_ASN1_OPEN_TYPE_STRUCT;
+
+typedef struct {
+   uint32_t numids;                             // number of subidentifiers
+   uint32_t subid[LIBLTE_ASN1_OID_MAXSUBIDS];   // subidentifier values
+} LIBLTE_ASN1_OID_STRUCT;
 
 typedef struct{
     bool   data_valid;
@@ -150,19 +176,48 @@ uint32 liblte_bits_2_value(uint8  **bits,
                            uint32   N_bits);
 
 /*********************************************************************
-    Name: pack
+    Name: liblte_pack
 
     Description: Pack a bit array into a byte array
 *********************************************************************/
-void pack(LIBLTE_BIT_MSG_STRUCT  *bits,
-          LIBLTE_BYTE_MSG_STRUCT *bytes);
+void liblte_pack(LIBLTE_BIT_MSG_STRUCT  *bits,
+                 LIBLTE_BYTE_MSG_STRUCT *bytes);
 
 /*********************************************************************
-    Name: unpack
+    Name: liblte_unpack
 
     Description: Unpack a byte array into a bit array
 *********************************************************************/
-void unpack(LIBLTE_BYTE_MSG_STRUCT *bytes,
-            LIBLTE_BIT_MSG_STRUCT  *bits);
+void liblte_unpack(LIBLTE_BYTE_MSG_STRUCT *bytes,
+                   LIBLTE_BIT_MSG_STRUCT  *bits);
+
+/*********************************************************************
+    Name: liblte_pack
+
+    Description: Pack a bit array into a byte array
+*********************************************************************/
+void liblte_pack(uint8_t *bits,  uint32_t n_bits, uint8_t *bytes);
+
+/*********************************************************************
+    Name: liblte_unpack
+
+    Description: Unpack a byte array into a bit array
+*********************************************************************/
+void liblte_unpack(uint8_t *bytes, uint32_t n_bytes, uint8_t *bits);
+
+/*********************************************************************
+    Name: liblte_align_up
+
+    Description: Aligns a pointer to a multibyte boundary
+*********************************************************************/
+void liblte_align_up(uint8_t **ptr, uint32_t align);
+
+/*********************************************************************
+    Name: liblte_align_up_zero
+
+    Description:  Aligns a pointer to a multibyte boundary and zeros
+                  bytes skipped
+*********************************************************************/
+void liblte_align_up_zero(uint8_t **ptr, uint32_t align);
 
 #endif /* __LIBLTE_COMMON_H__ */
--- liblte/src/liblte_common.cc
+++ liblte/src/liblte_common.cc
@@ -93,15 +93,15 @@ uint32 liblte_bits_2_value(uint8  **bits,
 }
 
 /*********************************************************************
-    Name: pack
+    Name: liblte_pack
 
     Description: Pack a bit array into a byte array
 *********************************************************************/
-void pack(LIBLTE_BIT_MSG_STRUCT  *bits,
-          LIBLTE_BYTE_MSG_STRUCT *bytes)
+void liblte_pack(LIBLTE_BIT_MSG_STRUCT  *bits,
+                 LIBLTE_BYTE_MSG_STRUCT *bytes)
 {
-    uint8* bit_ptr = bits->msg;
-    uint32 i;
+    uint8_t* bit_ptr = bits->msg;
+    uint32_t i;
 
     for(i=0; i<bits->N_bits/8; i++)
     {
@@ -116,19 +116,83 @@ void pack(LIBLTE_BIT_MSG_STRUCT  *bits,
 }
 
 /*********************************************************************
-    Name: unpack
+    Name: liblte_unpack
 
     Description: Unpack a byte array into a bit array
 *********************************************************************/
-void unpack(LIBLTE_BYTE_MSG_STRUCT *bytes,
-            LIBLTE_BIT_MSG_STRUCT  *bits)
+void liblte_unpack(LIBLTE_BYTE_MSG_STRUCT *bytes,
+                   LIBLTE_BIT_MSG_STRUCT  *bits)
 {
-    uint8 *bit_ptr = bits->msg;
-    uint32 i;
+    uint8_t *bit_ptr = bits->msg;
+    uint32_t i;
 
     for(i=0; i<bytes->N_bytes; i++)
     {
         liblte_value_2_bits(bytes->msg[i], &bit_ptr, 8);
     }
     bits->N_bits = bytes->N_bytes*8;
+}
+
+/*********************************************************************
+    Name: liblte_pack
+
+    Description: Pack a bit array into a byte array
+*********************************************************************/
+void liblte_pack(uint8_t *bits,  uint32_t n_bits, uint8_t *bytes)
+{
+    uint8_t* bit_ptr = bits;
+    uint32_t i;
+
+    for(i=0; i<n_bits/8; i++)
+    {
+        bytes[i] = liblte_bits_2_value(&bit_ptr, 8);
+    }
+    if(n_bits%8 > 0)
+    {
+        bytes[n_bits/8] = liblte_bits_2_value(&bit_ptr, n_bits%8);
+    }
+}
+
+/*********************************************************************
+    Name: liblte_unpack
+
+    Description: Unpack a byte array into a bit array
+*********************************************************************/
+void liblte_unpack(uint8_t *bytes, uint32_t n_bytes, uint8_t *bits)
+{
+    uint8_t *bit_ptr = bits;
+    uint32_t i;
+
+    for(i=0; i<n_bytes; i++)
+    {
+        liblte_value_2_bits(bytes[i], &bit_ptr, 8);
+    }
+}
+
+/*********************************************************************
+    Name: liblte_align_up
+
+    Description: Aligns a pointer to a multibyte boundary
+*********************************************************************/
+void liblte_align_up(uint8_t **ptr, uint32_t align)
+{
+  while( (uint64_t)(*ptr) % align > 0)
+  {
+    (*ptr)++;
+  }
+}
+
+/*********************************************************************
+    Name: liblte_align_up_zero
+
+    Description:  Aligns a pointer to a multibyte boundary and zeros
+                  bytes skipped
+*********************************************************************/
+void liblte_align_up_zero(uint8_t **ptr, uint32_t align)
+{
+  while( (uint64_t)(*ptr) % align > 0)
+  {
+    **ptr = 0;
+    (*ptr)++;
+  }
 }
